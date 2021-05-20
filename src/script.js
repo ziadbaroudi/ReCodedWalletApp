5cdc984468c95e51348e02d69270ad1bd254a553
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-21 00:59:32 +0300

    added wallet general form in html ~ hidden

--- src/script.js
+++ src/script.js
@@ -2,8 +2,12 @@ let createButton = document.getElementById("createBtnM")
 let walletName = document.getElementById("walletName")
 let balance = document.getElementById("balance")
 let desc = document.getElementById("desc")
+let container1 = document.getElementById("hidden1")
+let container2 = document.getElementById("hidden2")
 createButton.addEventListener('click', (e)=>{
     e.preventDefault()
+    container1.classList.add("d-none")
+    container2.classList.remove("d-none")
     console.log(desc.value);
 })
 
