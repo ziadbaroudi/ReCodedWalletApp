a8708bc95435b4908be9a5aaf1cf883e9d032ea2
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-22 01:28:43 +0300

    style changes

--- src/script.js
+++ src/script.js
@@ -4,10 +4,12 @@ let balance = document.getElementById("balance")
 let desc = document.getElementById("desc")
 let container1 = document.getElementById("hidden1")
 let container2 = document.getElementById("hidden2")
+let walletsNameDrop = document.getElementById("walletsNames")
 createButton.addEventListener('click', (e)=>{
     e.preventDefault()
     container1.classList.add("d-none")
     container2.classList.remove("d-none")
+    walletsNameDrop.classList.remove("d-none")
     console.log(desc.value);
 })
 
