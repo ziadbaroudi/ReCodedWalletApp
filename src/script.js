a9a78cfb2fdf90c6877725ad4cb8ddbfbd21a7dd
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-22 03:51:26 +0300

    js

--- src/script.js
+++ src/script.js
@@ -1,15 +1,54 @@
 let createButton = document.getElementById("createBtnM")
-let walletName = document.getElementById("walletName")
+let welcome = document.getElementById("hidden1")
+let wallet = document.getElementById("hidden2")
 let balance = document.getElementById("balance")
-let desc = document.getElementById("desc")
-let container1 = document.getElementById("hidden1")
-let container2 = document.getElementById("hidden2")
 let walletsNameDrop = document.getElementById("walletsNames")
-createButton.addEventListener('click', (e)=>{
+let walletBalance = document.getElementById("walletBalance")
+// removes welcome page when new wallet is added
+createButton.addEventListener("click", (e)=>{
     e.preventDefault()
-    container1.classList.add("d-none")
-    container2.classList.remove("d-none")
+    welcome.classList.add("d-none")
+    wallet.classList.remove("d-none")
     walletsNameDrop.classList.remove("d-none")
-    console.log(desc.value);
-})
+});
 
+class Wallet {
+    constructor(name, currency, balance = 0, desc) {
+        this.name = name;
+        this.currency = currency;
+        this.balance = balance;
+        this.desc = desc;
+    }
+
+    get balance(){
+        return this.balance;
+    }
+
+    set balance(bal) {
+        this.balance = bal;
+    }
+
+    render(){
+        console.log(balance())
+        walletBalance.innerHTML += balance()
+    }
+}
+
+
+
+class Transaction {
+
+}
+
+
+class Expense {
+
+}
+
+class Income {
+
+}
+
+class Currency {
+    
+}
